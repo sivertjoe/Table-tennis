ea1cfbb547fbd7cfeafabeb5f57a8f78fe8f2513
markhilb <mhilbertsen@hotmail.no>
2020-10-30 15:31:05 +0100

    Create search bar component

---
+++ website/src/components/search-bar/SearchBar.js
@@ -0,0 +1,24 @@
+import { React, Component } from 'react'
+import './SearchBar.css'
+
+class SearchBar extends Component {
+  callback = null
+
+  constructor(args) {
+    super()
+    this.callback = args.callback
+    this.search = this.search.bind(this)
+  }
+
+  search(event) {
+    this.callback(event.target.value)
+  }
+
+  render() {
+    return (
+      <input type="text" placeholder="Search for user" onChange={this.search} />
+    )
+  }
+}
+
+export default SearchBar
