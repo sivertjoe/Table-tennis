6f93bb86a3183ee7dee7badd8519ef46853e2be6
Markus Bergum Hilbertsen <mhilbertsen@hotmail.no>
2020-10-30 14:18:04 +0100

    Style profile page (#2)

--- website/src/pages/profile-page/ProfilePage.js
+++ website/src/pages/profile-page/ProfilePage.js
@@ -2,15 +2,47 @@ import { React, Component } from 'react'
 import * as Api from '../../api/Api'
 import './ProfilePage.css'
 
+function formatDate(ms) {
+  const d = new Date(ms)
+  return (
+    `${d.getFullYear()}/${d.getMonth()}/${d.getDate()} ` +
+    `${d.getHours()}:${d.getMinutes()}`
+  )
+}
+
 class Profile extends Component {
   constructor(user) {
     super()
-    this.username = user.user
+    Api.getUser(user.user).then((user) => {
+      this.setState({ user: user })
+    })
   }
 
   render() {
-    Api.getUser(this.username).then((user) => (this.user = user))
-      return <div>{this.user}</div>
+    console.log(this.state?.user)
+    return (
+      <div className="container">
+        <h1 className="name">{this.state?.user.name}</h1>
+        <h2 className="elo">{Math.trunc(this.state?.user.elo ?? 0)}</h2>
+        <h2 className="history">Match history</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th>Winner</th>
+              <th>Loser</th>
+              <th>Date</th>
+            </tr>
+            {this.state?.user.match_history.map((elem) => (
+              <tr>
+                <td>{elem.winner}</td>
+                <td>{elem.loser}</td>
+                <td>{formatDate(elem.epoch)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
