f6b9e44a8f42f01cc2dead9d943724a4c0a6d01f
markhilb <mhilbertsen@hotmail.no>
2020-11-02 11:57:01 +0100

    Make navbar small on small screens

--- website/src/components/navbar/Navbar.js
+++ website/src/components/navbar/Navbar.js
@@ -3,19 +3,20 @@ import './Navbar.css'
 
 class Navbar extends Component {
   render() {
+    const large = window.matchMedia('(min-width: 450px)').matches
     return (
       <div className="navbar">
         <div className="items">
-          <h2 className="item">
+          <h2 className={`item ${large ? '' : 'small'}`}>
             <a href="/">Home</a>
           </h2>
-          <h2 className="item">
+          <h2 className={`item ${large ? '' : 'small'}`}>
             <a href="/match">Match</a>
           </h2>
-          <h2 className="item">
+          <h2 className={`item ${large ? '' : 'small'}`}>
             <a href="/profiles">Profiles</a>
           </h2>
-          <h2 className="item">
+          <h2 className={`item ${large ? '' : 'small'}`}>
             <a href="/register">Register</a>
           </h2>
         </div>
