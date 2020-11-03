6a157de364b637be416654936931a37b3fc4b4c4
markhilb <mhilbertsen@hotmail.no>
2020-11-03 16:05:57 +0100

    Fix scrolling on table

--- website/src/pages/history/History.js
+++ website/src/pages/history/History.js
@@ -16,37 +16,40 @@ class History extends Component {
     return (
       <div className="container">
         <h1 className="center">Match History</h1>
-        <table>
-          <tbody>
-            <tr key={ranking}>
-              <th>Winner</th>
-              <th>New Elo</th>
-              <th>Loser</th>
-              <th>New Elo</th>
-            </tr>
-            {this.state?.history.map((match) => {
-              ranking += 1
-              return (
-                <tr key={ranking}>
-                  <td>
-                    <a href={'/profiles/' + match.winner}>{match.winner}</a>
-                  </td>
-                  <td>
-                    {Math.trunc(match.winner_elo)} (
-                    <div className="green">+{Math.trunc(match.elo_diff)}</div>)
-                  </td>
-                  <td>
-                    <a href={'/profiles/' + match.loser}>{match.loser}</a>
-                  </td>
-                  <td>
-                    {Math.trunc(match.loser_elo)} (
-                    <div className="red">-{Math.trunc(match.elo_diff)}</div>)
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+        <div className="table-container">
+          <table>
+            <tbody>
+              <tr key={ranking}>
+                <th>Winner</th>
+                <th>New Elo</th>
+                <th>Loser</th>
+                <th>New Elo</th>
+              </tr>
+              {this.state?.history.map((match) => {
+                ranking += 1
+                return (
+                  <tr key={ranking}>
+                    <td>
+                      <a href={'/profiles/' + match.winner}>{match.winner}</a>
+                    </td>
+                    <td>
+                      {Math.trunc(match.winner_elo)} (
+                      <div className="green">+{Math.trunc(match.elo_diff)}</div>
+                      )
+                    </td>
+                    <td>
+                      <a href={'/profiles/' + match.loser}>{match.loser}</a>
+                    </td>
+                    <td>
+                      {Math.trunc(match.loser_elo)} (
+                      <div className="red">-{Math.trunc(match.elo_diff)}</div>)
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
