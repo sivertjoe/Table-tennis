ebe1286c8d9c1506f10578f28e8806b458d4abac
Markus Bergum Hilbertsen <mhilbertsen@hotmail.no>
2020-12-07 12:32:43 +0100

    Use rustfmt

--- server/src/match.rs
+++ server/src/match.rs
@@ -1,41 +1,41 @@
-use serde_derive::{Serialize, Deserialize};
+use serde_derive::{Deserialize, Serialize};
 
 
 #[derive(Debug, Serialize)]
 pub struct Match
 {
-    pub winner: String,
-    pub loser: String,
-    pub epoch: i64,
-    pub elo_diff: f64,
+    pub winner:     String,
+    pub loser:      String,
+    pub epoch:      i64,
+    pub elo_diff:   f64,
     pub winner_elo: f64,
-    pub loser_elo: f64,
+    pub loser_elo:  f64,
 }
 
 #[derive(Debug, Serialize)]
 pub struct EditMatchInfo
 {
-    pub winner: String, 
-    pub loser: String,
-    pub epoch: i64, 
-    pub id: i64
+    pub winner: String,
+    pub loser:  String,
+    pub epoch:  i64,
+    pub id:     i64,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct DeleteMatchInfo
 {
-    pub id: i64,
+    pub id:    i64,
     pub token: String,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct NewEditMatchInfo
 {
-    pub winner: String, 
-    pub loser: String,
-    pub epoch: i64, 
-    pub id: i64,
-    pub token: String,
+    pub winner: String,
+    pub loser:  String,
+    pub epoch:  i64,
+    pub id:     i64,
+    pub token:  String,
 }
 
 // Match info sent from the front end
@@ -43,15 +43,14 @@ pub struct NewEditMatchInfo
 pub struct MatchInfo
 {
     pub winner: String,
-    pub loser: String,
-    pub token: String
+    pub loser:  String,
+    pub token:  String,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct MatchResponse
 {
     pub match_notification_id: i64,
-    pub ans: u8,
-    pub token: String,
+    pub ans:                   u8,
+    pub token:                 String,
 }
-
