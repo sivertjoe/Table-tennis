e9971b6303c48d50099406073e32090b53c93f45
Markus Bergum Hilbertsen <mhilbertsen@hotmail.no>
2020-12-30 11:35:53 +0100

    Export badges

---
+++ website/src/assets/images.js
@@ -0,0 +1,12 @@
+// TODO: Can this be done dynamically???
+import FirstPlace from './first_place.png'
+import SecondPlace from './second_place.png'
+import ThirdPlace from './third_place.png'
+
+const images = {
+  'first_place.png': FirstPlace,
+  'second_place.png': SecondPlace,
+  'third_place.png': ThirdPlace,
+}
+
+export default images
