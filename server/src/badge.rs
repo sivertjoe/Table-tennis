e63dda16ddfb686051bb5f9f5a061f7bf2132fd3
sivertjoe <sivert-joh@hotmail.com>
2020-12-30 10:20:47 +0100

    Use improved sprites for prizes

--- server/src/badge.rs
+++ server/src/badge.rs
@@ -6,10 +6,8 @@ pub struct Badge
     pub id:     i64,
     pub season: i64,
     pub name:   String,
-    pub color:  String,
 }
 
-
-pub static BADGES: &'static [&'static str] = &["crown", "medal", "medal", "award", "trophy"];
-pub static BADGE_COLORS: &'static [&'static str] =
-    &["yellow", "silver", "orange", "turquoise", "yellow"];
+pub const NUM_SEASON_PRIZES: usize = 3;
+pub static BADGES: &'static [&'static str] 
+    = &["first_place.png", "second_place.png", "third_place.png", "default_tournament_win.png"];
