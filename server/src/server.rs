d3c8464887d22d4383923c407567a8a356735167
sivertjoe <sivert-joh@hotmail.com>
2021-02-01 21:50:21 +0100

    Move macro

--- server/src/server.rs
+++ server/src/server.rs
@@ -17,6 +17,32 @@ use crate::{
     },
 };
 
+// Kneel before my one-liner
+#[macro_export]
+macro_rules! GET_OR_CREATE_DB_VAR {
+    ($conn: expr, $id: expr, $default_value: expr) => {
+        $conn
+            .prepare("select value from variables where id = :id")?
+            .query_map_named(named_params! {":id": $id}, |row| {
+                let c: i32 = row.get(0)?;
+                Ok(c)
+            })?
+            .next()
+            .map_or_else(
+                || {
+                    $conn
+                        .execute("insert into variables (id, value) values (?1, ?2)", params![
+                            $id,
+                            $default_value
+                        ])
+                        .expect(&format!("Inserting into variables <{}, {}>", $id, $default_value));
+                    Ok($default_value)
+                },
+                |val| Ok(val.unwrap()),
+            )
+    };
+}
+
 pub struct DataBase
 {
     pub conn: Connection,
