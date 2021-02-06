c7f85b67effe1ffde94a31e73036e53624eed069
sivertjoe <sivert-joh@hotmail.com>
2021-02-06 12:57:37 +0100

    Add season/off-season to leaderboard

--- website/src/components/leaderboard/Leaderboard.js
+++ website/src/components/leaderboard/Leaderboard.js
@@ -1,5 +1,6 @@
 import { React, Component } from 'react'
 import * as UserApi from '../../api/UserApi'
+import * as MatchApi from '../../api/MatchApi'
 import './Leaderboard.css'
 import '../../index.css'
 import Badges from '../badges/Badges'
@@ -7,17 +8,28 @@ import images from '../../assets/images'
 
 class Leaderboard extends Component {
   users = []
+  seasonText = ''
+  isSeason = false
 
   constructor() {
     super()
 
-    UserApi.getUsers()
-      .then((users) => (this.users = users))
+    MatchApi.getLeaderboardInfo()
+      .then((info) => {
+        this.users = info.users
+        this.isSeason = info.is_season
+        if (info.is_season) {
+          this.seasonText = 'Season: ' + info.season_number
+        } else {
+          this.seasonText = 'Off-season'
+        }
+      })
       .catch((error) => console.warn(error.message))
       .finally(() => this.setState({}))
   }
 
   getRankBadge(rank) {
+    if (!this.isSeason) return
     let badge
     if (rank === 1) badge = images['first_place.png']
     else if (rank === 2) badge = images['second_place.png']
@@ -33,6 +45,7 @@ class Leaderboard extends Component {
     return (
       <div className="container">
         <h1 className="center">Leaderboard</h1>
+        <h2 className="center">{this.seasonText}</h2>
         <div className="table-container">
           <table>
             <tbody>
