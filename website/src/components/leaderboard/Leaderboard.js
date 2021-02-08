d204cb3872f7a3124c1834e84cf8f3b662ba258a
Markus Bergum Hilbertsen <mhilbertsen@hotmail.no>
2021-02-08 14:36:24 +0100

    Finish stats page

--- website/src/components/leaderboard/Leaderboard.js
+++ website/src/components/leaderboard/Leaderboard.js
@@ -1,5 +1,4 @@
 import { React, Component } from 'react'
-import * as UserApi from '../../api/UserApi'
 import * as MatchApi from '../../api/MatchApi'
 import './Leaderboard.css'
 import '../../index.css'
