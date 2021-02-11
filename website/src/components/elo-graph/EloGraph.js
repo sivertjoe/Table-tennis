2c114bbea5b0168a2e370e2ce49c8b64e329702a
bernt <berntjulian@gmail.com>
2021-02-11 11:20:21 +0100

    Enhance the graph component

--- website/src/components/elo-graph/EloGraph.js
+++ website/src/components/elo-graph/EloGraph.js
@@ -2,7 +2,9 @@ import { React, Component } from 'react'
 import { ResponsiveLine } from '@nivo/line'
 import Select from 'react-select'
 import './EloGraph.css'
-import { getShortDate, sameDay, getPreviousDate } from '../../utils/Date'
+import { Defs } from '@nivo/core'
+import { area, curveMonotoneX } from 'd3-shape'
+import { getShortDate, getPreviousDate } from '../../utils/Date'
 
 function matchElo(match, name) {
   return match.winner === name ? match.winner_elo : match.loser_elo
@@ -11,69 +13,39 @@ function matchElo(match, name) {
 class EloGraph extends Component {
   periods = [
     {
-      value: 'day',
-      label: '1 Day',
+      value: 'today',
+      label: 'Today',
       ticks: 'every 1 day',
-      date: getPreviousDate(1),
+      date: getPreviousDate(0),
     },
     {
       value: 'week',
-      label: '1 Week',
+      label: 'Last Week',
       ticks: 'every 1 day',
       date: getPreviousDate(7),
     },
     {
       value: 'month',
-      label: '1 Month',
+      label: 'This Season',
       ticks: 'every 2 day',
       date: getPreviousDate(30),
     },
-    {
-      value: '3month',
-      label: '3 Month',
-      ticks: 'every 4 day',
-      date: getPreviousDate(91),
-    },
-    {
-      value: '6month',
-      label: '6 Month',
-      ticks: 'every 1 week',
-      date: getPreviousDate(182),
-    },
-    {
-      value: 'year',
-      label: '1 Year',
-      ticks: 'every 1 month',
-      date: getPreviousDate(365),
-    },
-    {
-      value: 'alltime',
-      label: 'All Time',
-      ticks: 'every 1 year',
-      date: new Date(0),
-    },
+    // {
+    //   value: 'alltime',
+    //   label: 'All Time',
+    //   ticks: 'every 1 year',
+    //   date: new Date(0),
+    // },
+    /* Qurey db for all time */
   ]
-  selectedPeriod = this.periods[2]
-
-  details = [
-    {
-      value: 'average',
-      label: 'Daily Average',
-    },
-    {
-      value: 'detailed',
-      label: 'Every Match',
-    },
-  ]
-  selectedDetail = this.details[0]
+  selectedPeriod = this.periods[1]
 
   constructor(args) {
     super()
     this.user = args.user
     this.changePeriod = this.changePeriod.bind(this)
-    this.changeDetail = this.changeDetail.bind(this)
     this.getEveryMatch = this.getEveryMatch.bind(this)
-    this.getDailyAverage = this.getDailyAverage.bind(this)
+    this.genLayers = this.genLayers.bind(this)
   }
 
   changePeriod(e) {
@@ -81,96 +53,120 @@ class EloGraph extends Component {
     this.setState({})
   }
 
-  changeDetail(e) {
-    this.selectedDetail = e
-    this.setState({})
-  }
-
   getEveryMatch() {
     const name = this.user.name
+    let x = 0
     return this.user.match_history?.reduceRight((res, match, i) => {
-      if (match.epoch > this.selectedPeriod.date.getTime())
+      if (match.epoch > this.selectedPeriod.date.getTime()) {
+        console.log(match)
         res.push({
-          x: new Date(match.epoch),
           y: Math.round(matchElo(match, name)),
+          x: x,
+          time: new Date(match.epoch),
+          info: [match.winner, match.loser],
+          elo_diff: Math.round(match.elo_diff),
         })
+        x++
+      }
       return res
     }, [])
   }
 
-  getDailyAverage() {
-    const name = this.user.name
-    const history = this.user.match_history
+  genLayers(flag_change_col, day) {
+    const Layer = ({ xScale, innerHeight }) => {
+      const areaGenerator = area()
+        .x((d) => xScale(d.x))
+        .y0(0)
+        .y1(innerHeight)
+        .curve(curveMonotoneX)
 
-    // Find the last match within the selected range
-    const end = this.selectedPeriod.date.getTime()
-    const startIdx = history.findIndex((match) => match.epoch < end)
-    let idx = (startIdx < 0 ? history.length : startIdx) - 1
+      const [color, id, fill] = flag_change_col
+        ? ['#ff8c00', '1', 'url(#1)']
+        : ['orange', '2', 'url(#2)']
 
-    // If there are no matches within the selected range
-    if (idx <= 0) return []
-
-    const last = history[idx]
-    let currentDate = new Date(last.epoch)
-    let sum = matchElo(last, name)
-    let count = 1
-
-    let matchDate = new Date(last.epoch)
-    let elo = sum
-
-    let res = []
-    const tomorrow = getPreviousDate(-1).getTime()
-    while (currentDate.getTime() < tomorrow) {
-      // If there were no matches on currentDate
-      if (!sameDay(matchDate, currentDate) || idx <= 0) {
-        // Add the average elo of that day, and go to next day
-        res.push({
-          x: new Date(currentDate),
-          y: Math.round(sum / count),
-        })
-        currentDate.setDate(currentDate.getDate() + 1)
-        sum = elo
-        count = 1
-      }
-      // If the match was on currentDate
-      else {
-        // Add the match, and go to next match
-        sum += elo
-        count++
-        idx--
-        matchDate = new Date(history[idx].epoch)
-        elo = matchElo(history[idx], name)
-      }
+      return (
+        <>
+          <Defs
+            defs={[
+              {
+                id: id,
+                type: 'patternLines',
+                background: 'transparent',
+                color: color,
+                lineWidth: 1,
+                spacing: 3,
+                rotation: -45,
+              },
+            ]}
+          />
+          <path
+            d={areaGenerator(day)}
+            fill={fill}
+            fillOpacity={0.2}
+            stroke="var(--primary-color)"
+            strokeWidth={0.5}
+            strokeOpacity={0.2}
+          />
+        </>
+      )
     }
-
-    return res
+    return Layer
   }
 
   render() {
     const items = [
       {
         id: 0,
-        data:
-          this.selectedDetail.value === 'average'
-            ? this.getDailyAverage()
-            : this.getEveryMatch(),
-      },
+        data: this.getEveryMatch(),
+      } /* TODO: Insert opponent's graph here */,
     ]
 
-    // If there is no history to show, just show current elo
-    if (!items[0].data.length)
-      items[0].data = [{ x: new Date(), y: Math.round(this.user.elo) }]
+    let minElo = this.user.elo,
+      maxElo = this.user.elo
 
+    let layers = []
+    // If there is no history to show, just show current elo
+    if (!items[0].data.length) {
+      items[0].data = [{ time: new Date(), x: 0, y: Math.round(this.user.elo) }]
+    } else {
+      //min/max elo for upper and lower y value
+      for (let i = 1; i < items[0].data.length; i++) {
+        let value = items[0].data[i]
+        minElo = value.y < minElo ? value.y : minElo
+        maxElo = value.y > maxElo ? value.y : maxElo
+      }
+
+      //norm the timestamps in order to put them in the same grid
+      items[0].data.forEach((d) => {
+        d.dateTime = new Date(d.time.toDateString())
+      })
+
+      let first_match = items[0].data[0]
+      let last_match = items[0].data[0]
+      let col = true
+
+      //send first match and last match of the current day to in order for generating layer for that day
+      for (let i = 0; i < items[0].data.length; i++) {
+        if (first_match.dateTime.getTime() === last_match.dateTime.getTime()) {
+          last_match = items[0].data[i + 1]
+        } else {
+          col = !col
+          layers.push(this.genLayers(col, [first_match, last_match]))
+          first_match = last_match
+        }
+      }
+      col = !col
+      layers.push(
+        this.genLayers(col, [
+          first_match,
+          items[0].data[items[0].data.length - 1],
+        ]),
+      )
+    }
     return (
       <>
         <h2>Elo history</h2>
         <div className="inputs">
-          <Select
-            className="selector"
-            onChange={this.changeDetail}
-            options={this.details}
-            value={this.selectedDetail}
-          />
           <Select
             className="selector"
             onChange={this.changePeriod}
@@ -181,37 +177,50 @@ class EloGraph extends Component {
         <div style={{ width: '100%', height: '600px' }}>
           <ResponsiveLine
             data={items}
-            margin={{ top: 50, right: 50, bottom: 100, left: 50 }}
+            margin={{ top: 50, right: 10, bottom: 100, left: 40 }}
             xScale={{
-              type: 'time',
+              type: 'linear',
               format: 'native',
+              max: 'auto',
+              min: '0',
             }}
             yScale={{
-              type: 'linear',
-              min: 'auto',
-              max: 'auto',
+              type: items[0].data.length === 1 ? 'point' : 'linear',
+              max: maxElo + 25,
+              min: minElo - 25,
             }}
             axisBottom={{
-              tickValues: this.selectedPeriod.ticks,
               orient: 'bottom',
-              tickSize: 5,
+              tickSize: 0,
+              format: () => null,
               tickPadding: 5,
               tickRotation: 45,
-              format: '%d %b %Y',
+              legendPosition: 'middle',
+              legendOffset: 40,
             }}
             axisLeft={{
               orient: 'left',
               tickSize: 5,
               tickPadding: 5,
             }}
-            colors={{ scheme: 'nivo' }}
             pointSize={4}
             pointColor={{ theme: 'background' }}
             pointBorderWidth={2}
             pointBorderColor={{ from: 'serieColor' }}
             useMesh={true}
-            enableGridX={false}
-            enableGridY={false}
+            layers={[
+              ...layers,
+              'markers',
+              'areas',
+              'lines',
+              'slices',
+              'axes',
+              'points',
+              'legends',
+              'crosshair',
+              'mesh',
+            ]}
+            // animate={false} //#TODO: find out if only some parts can animate
             theme={{
               textColor: 'var(--primary-color)',
               background: 'var(--black)',
@@ -240,8 +249,21 @@ class EloGraph extends Component {
             }}
             tooltip={(input) => (
               <div className="tooltip">
-                <div>{input.point.data.y}</div>
-                <div>{getShortDate(input.point.data.x)}</div>
+                <div>
+                  {input.point.data.info
+                    ? 'W: ' +
+                      input.point.data.info[0] +
+                      ', L: ' +
+                      input.point.data.info[1]
+                    : ''}
+                </div>
+                <div>
+                  {input.point.data.y}{' '}
+                  {input.point.data.elo_diff
+                    ? '(+' + input.point.data.elo_diff + ')'
+                    : ''}
+                </div>
+                <div>{getShortDate(input.point.data.time)}</div>
               </div>
             )}
           ></ResponsiveLine>
