f126fdf8663a0281503a6269de1c7332564a8ba8
Markus Bergum Hilbertsen <mhilbertsen@hotmail.no>
2021-05-07 12:22:20 +0200

    Refactor

--- website/src/pages/create-tournament/CreateTournament.js
+++ website/src/pages/create-tournament/CreateTournament.js
@@ -3,45 +3,17 @@ import '../../index.css'
 import './CreateTournament.css'
 import * as Api from '../../api/TournamentApi'
 import ImageUpload from '../../components/image-upload/ImageUpload'
-import images from '../../assets/images'
+import Button from '../../components/button/Button'
+import Input from '../../components/input/Input'
 
 const maxSize = 256 * 256
 
-function srcToFile(src, fileName, mimeType) {
-  return fetch(src)
-    .then(function (res) {
-      return res.arrayBuffer()
-    })
-    .then(function (buf) {
-      return new File([buf], fileName, { type: mimeType })
-    })
-}
-
-function getDefaultTouramentPrize() {
-  return srcToFile(images['tournament.png'], 'image.png', 'image/png')
-}
-
-function createTournament(image, name, numPlayers, setInfo, setColor) {
+const createTournament = (image, name, numPlayers, setInfo, setColor) => {
   setColor('red')
   if (!name) {
-    setInfo('Tournament need a name')
-    return
+    return setInfo('Tournament need a name')
   } else if (numPlayers === undefined || numPlayers < 3) {
-    setInfo('Tournament player count needs to be at least 4')
-    return
-  }
-
-  if (typeof image.name !== 'string') {
-    getDefaultTouramentPrize().then((newImage) => {
-      Api.createTournament(name, numPlayers, newImage)
-        .then(() => {
-          setColor('green')
-          setInfo('Success!')
-        })
-        .catch((e) => console.warn('Jaha' + e))
-    })
-
-    return
+    return setInfo('Tournament player count needs to be at least 4')
   }
 
   Api.createTournament(name, numPlayers, image)
@@ -49,54 +21,48 @@ function createTournament(image, name, numPlayers, setInfo, setColor) {
       setColor('green')
       setInfo('Success!')
     })
-    .catch((e) => console.warn('Jaha' + e))
+    .catch((e) => setInfo(e.toString()))
 }
 
 export default function CreateTournament() {
   const [name, setName] = useState('')
   const [numPlayers, setNumPlayers] = useState(0)
-  const [image, setImage] = useState([])
+  const [image, setImage] = useState(false)
   const [info, setInfo] = useState('')
   const [color, setColor] = useState('red')
 
   return (
-    <div className="container">
+    <div className="container center">
       <h1>Create a tournament</h1>
       <div className="inputs">
-        <input
-          className="input"
+        <Input
+          style={{ marginBottom: '1rem' }}
           type="text"
           placeholder="Tournament name"
-          onChange={(e) => setName(e.target.value)}
+          onChange={setName}
         />
-        <br />
-        <br />
-        <input
-          className="input"
+        <Input
           type="number"
           placeholder="Number of players"
-          onChange={(e) => setNumPlayers(e.target.value)}
+          onChange={setNumPlayers}
         />
-        <br />
-        <br />
-        <p>Choose prize, or don't upload to use the default</p>
-        <ImageUpload onUpload={(i) => setImage(i)} maxSize={maxSize} />
-        <p>
-          (Note: by creating a tournament <i>only you</i> can register games.
-          <br />
-          To do so, press and hold on the winner in each bracket)
-        </p>
-        <div className="button">
-          <input
-            value="Create Tournament"
-            className="big-button"
-            type="button"
-            onClick={() =>
-              createTournament(image, name, numPlayers, setInfo, setColor)
-            }
-          ></input>
+        <h2 style={{ margin: '2rem auto 0.5rem auto' }}>Choose prize</h2>
+        <p>Leave empty to use default prize</p>
+        <ImageUpload onUpload={setImage} maxSize={maxSize} />
+        <div
+          className="button"
+          onClick={() =>
+            createTournament(image, name, numPlayers, setInfo, setColor)
+          }
+        >
+          <Button placeholder="Create Tournament" />
         </div>
-        {info && <p style={{ color: color }}>{info}</p>}
+        <p>
+          Note: by creating a tournament <i>only you</i> can register games.
+          <br />
+          To do so, press and hold on the winner in each bracket
+        </p>
+        {info && <h2 style={{ color: color }}>{info}</h2>}
       </div>
     </div>
   )
