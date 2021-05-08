4843d60667a32ec35f121ed8f8f344640487270f
Markus Bergum Hilbertsen <mhilbertsen@hotmail.no>
2021-05-08 17:31:30 +0200

    Convert to component

--- website/src/components/tournament-list/TournamentList.js
+++ website/src/components/tournament-list/TournamentList.js
@@ -1,108 +1,97 @@
-import React, { useState } from 'react'
+import { React, Component } from 'react'
 import '../../index.css'
 import * as Api from '../../api/TournamentApi'
 import './TournamentList.css'
 import Button from '../../components/button/Button'
 
-function removeUser(users, setUsers, setInfo, setColor) {
-  const name = localStorage.getItem('username')
-  const index = users.indexOf(name)
-  if (index < 0) {
-    setColor('red')
-    setInfo('User not in tournament')
-  } else {
-    users.splice(index, 1)
-    setUsers(users)
-    setColor('green')
-    setInfo('Left tournament!')
+class TournamentList extends Component {
+  constructor(args) {
+    super()
+    this.state = {
+      info: '',
+      color: 'green',
+      tournament: args.tournament,
+      users: args.tournament.data.Players,
+    }
+
+    this.join = this.join.bind(this)
+    this.leave = this.leave.bind(this)
+    this.removeUser = this.removeUser.bind(this)
+  }
+
+  removeUser() {
+    const name = localStorage.getItem('username')
+    const index = this.state.users.indexOf(name)
+    if (index < 0) {
+      this.setState({ color: 'red', info: 'User not in tournament' })
+    } else {
+      this.state.users.splice(index, 1)
+      this.setState({ color: 'green', info: 'Left tournament!' })
+    }
+  }
+
+  leave(id) {
+    Api.leaveTournament(id)
+      .then(() => this.removeUser())
+      .catch((e) => this.setState({ color: 'red', info: e.toString() }))
+  }
+
+  join(id) {
+    Api.joinTournament(id)
+      .then(() => {
+        const name = localStorage.getItem('username')
+        this.setState({
+          users: [...this.state.users, name],
+          color: 'green',
+          info: 'Joined tournament!',
+        })
+      })
+      .catch((e) => this.setState({ color: 'red', info: e.toString() }))
+  }
+
+  render() {
+    const id = this.state.tournament.tournament.id
+    const tournamentName = this.state.tournament.tournament.name
+    const numPlayers = this.state.tournament.tournament.player_count
+
+    const list = this.state.users.map((name, index) => (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          {index + 1}/{numPlayers}
+        </td>
+      </tr>
+    ))
+
+    return (
+      <>
+        <h1>'{tournamentName}' Participants:</h1>
+        <div className="table-container">
+          <table>
+            <tbody>
+              <tr key={0}>
+                <th>User</th>
+                <th>Player Count</th>
+              </tr>
+              {list}
+            </tbody>
+          </table>
+        </div>
+        <br />
+
+        <div className="center" onClick={() => this.join(id)}>
+          <Button placeholder="Join" />
+        </div>
+        <div className="center" onClick={() => this.leave(id)}>
+          <Button placeholder="Leave" />
+        </div>
+        <div className="center">
+          {this.state.info && (
+            <h2 style={{ color: this.state.color }}>{this.state.info}</h2>
+          )}
+        </div>
+      </>
+    )
   }
 }
-
-function leave(id, users, setUsers, setInfo, setColor) {
-  Api.leaveTournament(id)
-    .then(() => {
-      removeUser(users, setUsers, setInfo, setColor)
-    })
-    .catch((e) => {
-      setColor('red')
-      setInfo(e.toString())
-    })
-}
-
-function join(id, users, setUsers, count, numPlayers, setInfo, setColor) {
-  Api.joinTournament(id)
-    .then(() => {
-      const name = localStorage.getItem('username')
-      setUsers([...users, name])
-      setColor('green')
-      setInfo('Success!')
-    })
-    .catch((e) => {
-      setColor('red')
-      setInfo(e.toString())
-    })
-}
-
-export default function TournamentList(args) {
-  const tournament = {
-    data: { Players: ['Markus', 'Bernt', 'Ella'] },
-    tournament: {
-      id: 1,
-      name: 'Epic',
-      player_count: 8,
-      prize: 'assets/tournament_badges/1.png',
-      state: 0,
-    },
-  }
-  const count = tournament.data.Players.length
-  const id = tournament.tournament.id
-  const tournamentName = tournament.tournament.name
-  const numPlayers = tournament.tournament.player_count
-  const [info, setInfo] = useState('')
-  const [color, setColor] = useState('red')
-  const [users, setUsers] = useState(tournament.data.Players)
-
-  const list = users.map((name, index) => (
-    <tr key={name}>
-      <td>{name}</td>
-      <td>
-        {index + 1}/{numPlayers}
-      </td>
-    </tr>
-  ))
-  return (
-    <>
-      <h1>'{tournamentName}' Participants:</h1>
-      <div className="table-container">
-        <table>
-          <tbody>
-            <tr key={0}>
-              <th>User</th>
-              <th>Player Count</th>
-            </tr>
-            {list}
-          </tbody>
-        </table>
-      </div>
-      <br />
-
-      <div
-        className="center"
-        onClick={(e) =>
-          join(id, users, setUsers, count, numPlayers, setInfo, setColor)
-        }
-      >
-        <Button placeholder="Join" />
-      </div>
-      <div
-        className="center"
-        onClick={(e) => leave(id, users, setUsers, setInfo, setColor)}
-      >
-        <Button placeholder="Leave" />
-      </div>
-      <div className="center">
-        {info && <h2 style={{ color: color }}>{info}</h2>}
-      </div>
-    </>
-  )
-}
+export default TournamentList
