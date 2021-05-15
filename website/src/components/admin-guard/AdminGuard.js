4d38d8a862af7f493f76cf91b50dabd0c0b4de56
sivertjoe <sivert-joh@hotmail.com>
2021-05-15 11:33:52 +0200

    add: use functional component in AdminGuard

--- website/src/components/admin-guard/AdminGuard.js
+++ website/src/components/admin-guard/AdminGuard.js
@@ -1,37 +1,32 @@
-import { React, Component } from 'react'
+import { React, useState, useEffect } from 'react'
 import * as AdminApi from '../../api/AdminApi'
 import '../../index.css'
 
-class AdminGuard extends Component {
-  isAdmin = 0
-  constructor() {
-    super()
+export default function AdminGuard(props) {
+  const [isAdmin, setIsAdmin] = useState(0)
+
+  useEffect(() => {
     const token = localStorage.getItem('token')
     if (token) {
       AdminApi.isAdmin(token)
         .then((isAdmin) => {
           if (isAdmin) {
-            this.isAdmin = 1
+            setIsAdmin(1)
           } else {
-            this.isAdmin = -1
+            setIsAdmin(-1)
           }
         })
         .catch((error) => console.warn(error.message))
-        .finally(() => this.setState({}))
-    } else this.isAdmin = -1
-  }
+    } else setIsAdmin(-1)
+  }, [])
 
-  render() {
-    if (this.isAdmin === 1) {
-      return this.props.children
-    } else if (this.isAdmin === -1)
-      return (
-        <div>
-          <img className="arnold" alt="STOP!!!" src={'../unauth.png'} />
-        </div>
-      )
-    else return <h1 style={{ textAlign: 'center' }}>Loading...</h1>
-  }
+  if (isAdmin === 1) {
+    return props.children
+  } else if (isAdmin === -1)
+    return (
+      <div>
+        <img className="arnold" alt="STOP!!!" src={'../unauth.png'} />
+      </div>
+    )
+  else return <h1 style={{ textAlign: 'center' }}>Loading...</h1>
 }
-
-export default AdminGuard
